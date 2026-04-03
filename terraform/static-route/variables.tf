ced3ec72cc9b2292cd60d2a9d69e47c41ce8d641
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-03 19:37:04 +0530

    fix: module level fixes

--- terraform/static-route/variables.tf
+++ terraform/static-route/variables.tf
@@ -8,9 +8,9 @@ variable "network_config" {
   description = "VPC details where VPN will be attached"
 
   type = object({
-    vpc_id = string
-    vpc_cidr = string
-    subnet_ids = list(string)
+    vpc_id = "vpc-0004d3466fc741795"
+    vpc_cidr = "10.20.0.0/16"
+    subnet_ids = ["subnet-02c09ab23118357e6"]
   })
 }
 
@@ -18,8 +18,8 @@ variable "vpn_config" {
   description = "Customer gateway and VPN configuration"
 
   type = object({
-    customer_gateway_ip = string
-    bgp_asn             = number
+    customer_gateway_ip = "10.0.0.1"
+    bgp_asn             = 65000
   })
 }
 
@@ -32,10 +32,10 @@ variable "enable_static_route" {
 variable "static_routes" {
   description = "List of destination CIDRs for static routing"
   type        = list(string)
-  default     = []
+  default     = ["192.168.0.0/16"]
 }
 
 variable "route_table_ids" {
   description = "Route tables for VPN propagation"
-  type        = list(string)
+  type        = ["rtb-022e4a40cca43caa8"]
 }
