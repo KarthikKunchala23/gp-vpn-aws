ccf76906937e6feb7785b68fc0e500dbadaf88b7
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-04 12:04:33 +0530

    fix(version): version tag changed

--- terraform/static-route/main.tf
+++ terraform/static-route/main.tf
@@ -1,5 +1,5 @@
 module "vpn_static_routes" {
-    source = "git::https://github.com/KarthikKunchala23/gp-vpn-aws.git//modules/vpn?ref=v1.1.3"
+    source = "git::https://github.com/KarthikKunchala23/gp-vpn-aws.git//modules/vpn?ref=v1.1.5"
 
     team_name = var.team_name
     network_config = {
