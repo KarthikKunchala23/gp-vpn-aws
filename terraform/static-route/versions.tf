d228e525bbd0a422b688ee247e7a34baed525b4a
Karthik Raju Kunchala <126945658+KarthikKunchala23@users.noreply.github.com>
2026-04-04 11:39:35 +0530

    fix(stateFile): Added Backend state config (#9)

--- terraform/static-route/versions.tf
+++ terraform/static-route/versions.tf
@@ -7,4 +7,12 @@ terraform {
       version = "6.38.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "gp-terraform-state-vpn"
+    key            = "static-route/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
 }
