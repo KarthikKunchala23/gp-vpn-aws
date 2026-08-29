14843e7d3b9cabef6c4679653950b0148e561453
karthik <karthikkunchala2398@gmail.com>
2026-08-29 14:41:12 +0530

    added missing region param in backend

--- terraform/dev/catalog-app-peer/main.tf
+++ terraform/dev/catalog-app-peer/main.tf
@@ -1,9 +1,9 @@
 module "rds-to-eks-peer" {
     source = "../../../modules/peering"
     peer_owner_id = "897722700244"
-    vpc_id = "vpc-0a1b2c3d4e5f6g7h8"
-    peer_vpc_id = "vpc-1a2b3c4d5e6f7g8h9"
+    vpc_id = "vpc-07604cc8c3e60d71f"
+    peer_vpc_id = "vpc-0d6dbb80bdcd558d0"
     auto_accept = true
     allow_remote_vpc_dns_resolution = true
-    name = "rds-to-eks-peering"
+    name = "retail-store-to-eks-peering"
 }
--- terraform/dev/catalog-app-peer/versions.tf
+++ terraform/dev/catalog-app-peer/versions.tf
@@ -12,6 +12,7 @@ terraform {
     key = "catalog-vpc-peering/terraform.tfstate"
     use_lockfile = true
     encrypt = true
+    region = "ap-south-1"
   }
 }
 
